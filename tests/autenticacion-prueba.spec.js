2ebf86d1c2a44ce15262d1b8559fa5578e0d257b
Michael Castillo <michaelsteven3211@gmail.com>
2025-04-28 18:51:59 -0500

    Organizar en carpetas futuras

--- tests/autenticacion-prueba.spec.js
+++ tests/autenticacion-prueba.spec.js
@@ -6,5 +6,5 @@ test.use({
 test('Ingreasr a la página ya autenticada', async ({ page }) => {
     await page.goto('https://practicetestautomation.com/logged-in-successfully/');
 
-    await expect(page.getByText('Logged In Successfully')).toBeVisible()
+    await expect(page.getByText('Logged In Successfully')).toBeVisible() // Verifica que el texto esté visible en la página
 })
